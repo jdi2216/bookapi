a767c79460ec2438844b733d9096ed67832d31d3
name <vvincentlaw@yandex.by>
2020-04-10 21:58:44 +0300

    add search by name

--- src/main/java/com/spring/bookapi/repository/BookRepository.java
+++ src/main/java/com/spring/bookapi/repository/BookRepository.java
@@ -5,8 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @RestResource(path = "category")
     Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    @RestResource(path = "department")
     Page<Book> findByDepartmentId(@Param("id") Long id, Pageable pageable);
+    @RestResource(path = "searchbykeyword")
+    Page<Book> findByNameContaining(@Param("name") String keyword, Pageable pageable);
 }
